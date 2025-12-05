5b8be80996b976ec3bcee4d976986fd2558406c4
Rozsavolgyi Zeteny Istvan <rozsavolgyi.zeteny@BOLYAI.LOCAL>
2025-12-05 15:41:25 +0100

    dummy adatok megírása (messages)

---
+++ backend/src/message/data.ts
@@ -0,0 +1,32 @@
+const datas = [
+    {
+        messageId: 1,
+        content: "üzenet, üzenet",
+        sentAt: "2025.08.11",
+        receiver: "nagy.eva",
+        sender: "toth.sandor"
+    },
+    {
+        messageId: 2,
+        content: "üzenet, üzenet",
+        sentAt: "2025.09.30",
+        receiver: "nagy.eva",
+        sender: "major.zsuzsanna"
+    },
+    {
+        messageId: 3,
+        content: "üzenet, üzenet",
+        sentAt: "2025.10.18",
+        receiver: "janos.peter",
+        sender: "toth.sandor"
+    },
+    {
+        messageId: 4,
+        content: "üzenet, üzenet",
+        sentAt: "2025.01.12",
+        receiver: "toth.sandor",
+        sender: "nagy.eva"
+    },
+];
+
+export default datas;
