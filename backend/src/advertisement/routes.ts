5085dc5dcf9e86c6c3cc07f4c611600725e565da
Zatony <152016613+Zatony@users.noreply.github.com>
2025-12-25 14:35:35 +0100

    GET ads végpontok hozzáadása

--- backend/src/advertisement/routes.ts
+++ backend/src/advertisement/routes.ts
@@ -6,9 +6,11 @@ const router: Router = Router();
 router.get('/backstagegear', getAds)
 router.get('/backstagegear/ads', getLatestAds);
 router.get('/backstagegear/ads/:adId', getAdDatasById);
-router.get('/backstagegear/:userId/ads', getUserAds);
-router.get('/backstagegear/:userId/ads/:adId', getUserAdById);
-router.get('/backstagegear/:userId/update_ad/:adId', getUserAdById);
+router.get('/backstagegear/:userId/ads', getAds);
+router.get('/backstagegear/:userId/ads/:adId', getAdDatasById);
+router.get('/backstagegear/:userId/my_ads', getUserAds);
+router.get('/backstagegear/:userId/my_ads/:adId', getUserAdById);
+router.get('/backstagegear/:userId/my_ads/update_ad/:adId', getUserAdById);
 
 // Csak adminnál kéne, hogy működjön
 router.get('/backstagegear/:userId/reported_ads', getReportedAds);
